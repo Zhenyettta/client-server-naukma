a1a0ff5d867d0e14f9589313bd90339ee533e9c8
Zhenyettta <zhecky2004@gmail.com>
2023-06-04 21:01:23 +0300

    Fixed tictak 😎

---
+++ tictak/Main.java
@@ -0,0 +1,19 @@
+package lection2.tictak;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+    public static void main(String[] args) {
+        ReentrantLock rel = new ReentrantLock();
+        Condition condition = rel.newCondition();
+        Data d = new Data();
+
+
+        new Worker(1, d, rel, condition);
+        new Worker(2, d, rel, condition);
+        new Worker(3, d, rel, condition);
+
+
+    }
+}
