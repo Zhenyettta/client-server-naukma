4595448133dc42aa0fd6c0aff0d14b8a4eee8d2c
Zhenyettta <zhecky2004@gmail.com>
2023-06-15 22:49:48 +0300

    fixed Processor.java

--- src/main/java/org/zhenyok/logic/Processor.java
+++ src/main/java/org/zhenyok/logic/Processor.java
@@ -15,6 +15,7 @@ public class Processor {
     private static final int NUM_THREADS = 10;
     private static final Lock productLock = new ReentrantLock();
 
+
     public static void process(Message message) {
         int command = message.getCommand();
         switch (command) {
@@ -64,93 +65,113 @@ public class Processor {
     }
 
     private static void getProductCount(Message message) {
-        Product product = getProductByName(new String(message.getDataBytes()));
+        productLock.lock();
+        try {
+            Product product = getProductByName(new String(message.getDataBytes()));
 
-        if (product != null) {
-            int count = product.getCount();
-            message.setMessageBytes(("Count of your product = " + count).getBytes());
-        } else {
-            message.setMessageBytes("Product not found".getBytes());
+            if (product != null) {
+                int count = product.getCount();
+                message.setMessageBytes(("Count of your product = " + count).getBytes());
+            } else {
+                message.setMessageBytes("Product not found".getBytes());
+            }
+        } finally {
+            productLock.unlock();
         }
     }
 
     private static void removeProduct(Message message) {
-        Product product = getProductByName(new String(message.getDataBytes()));
+        productLock.lock();
+        try {
+            Product product = getProductByName(new String(message.getDataBytes()));
 
-        if (product != null) {
-            productLock.lock();
-            try {
+            if (product != null) {
                 int countToRemove = message.getCount();
                 int currentCount = product.getCount();
                 int newCount = Math.max(currentCount - countToRemove, 0);
                 int removedCount = currentCount - newCount;
                 product.setCount(newCount);
                 message.setMessageBytes(("Removed " + removedCount + " products, actual count = " + newCount).getBytes());
-            } finally {
-                productLock.unlock();
+            } else {
+                message.setMessageBytes("Product not found".getBytes());
             }
-        } else {
-            message.setMessageBytes("Product not found".getBytes());
+        } finally {
+            productLock.unlock();
         }
     }
 
     private static void addProduct(Message message) {
+        productLock.lock();
+        try {
+            Product product = getProductByName(new String(message.getDataBytes()));
 
-        Product product = getProductByName(new String(message.getDataBytes()));
-
-        if (product != null) {
-            productLock.lock();
-            try {
+            if (product != null) {
                 product.setCount(product.getCount() + message.getCount());
                 message.setMessageBytes(("Added " + message.getCount() + " products, actual count = " + product.getCount()).getBytes());
-            } finally {
-                productLock.unlock();
+            } else {
+                message.setMessageBytes("Product not found".getBytes());
             }
-        } else {
-            message.setMessageBytes("Product not found".getBytes());
+        } finally {
+            productLock.unlock();
         }
     }
 
     private static void createGroup(Message message) {
-        if (getGroupByName(new String(message.getDataBytes())) == null) {
-            Group group = new Group(new String(message.getDataBytes()), Collections.synchronizedList(new ArrayList<>()));
-            Group.groups.add(group);
-            message.setMessageBytes(("Group " + new String(message.getDataBytes()) + " was created").getBytes());
-        } else
-            message.setMessageBytes("Group with this name already exists".getBytes());
+        productLock.lock();
+        try {
+            if (getGroupByName(new String(message.getDataBytes())) == null) {
+                Group group = new Group(new String(message.getDataBytes()), Collections.synchronizedList(new ArrayList<>()));
+                Group.groups.add(group);
+                message.setMessageBytes(("Group " + new String(message.getDataBytes()) + " was created").getBytes());
+            } else {
+                message.setMessageBytes("Group with this name already exists".getBytes());
+            }
+        } finally {
+            productLock.unlock();
+        }
     }
 
     private static void addProductToGroup(Message message) {
-        String[] data = new String(message.getDataBytes()).split(" ");
-        String productName = data[0];
-        String groupName = data[1];
+        productLock.lock();
+        try {
+            String[] data = new String(message.getDataBytes()).split(" ");
+            String productName = data[0];
+            String groupName = data[1];
 
-        Product product = getProductByName(productName);
-        Group group = getGroupByName(groupName);
+            Product product = getProductByName(productName);
+            Group group = getGroupByName(groupName);
 
-        if (group != null && !group.getProducts().contains(product) && product != null) {
-            synchronized (group) {
-                group.addProduct(product);
-                product.setGroup(group);
+            if (group != null && !group.getProducts().contains(product) && product != null) {
+                synchronized (group) {
+                    group.addProduct(product);
+                    product.setGroup(group);
+                }
+
+                message.setMessageBytes(("Product " + productName + " was successfully added to " + groupName).getBytes());
+            } else {
+                message.setMessageBytes("Product or group not found or product already in this group".getBytes());
             }
-
-            message.setMessageBytes(("Product " + productName + " was successfully added to " + groupName).getBytes());
-        } else {
-            message.setMessageBytes("Product or group not found or product already in this group".getBytes());
+        } finally {
+            productLock.unlock();
         }
     }
 
     private static void changeProductPrice(Message message) {
-        Product product = getProductByName(new String(message.getDataBytes()));
+        productLock.lock();
+        try {
+            Product product = getProductByName(new String(message.getDataBytes()));
 
-        if (product != null) {
-            synchronized (product) {
-                product.setPrice(message.getPrice());
+            if (product != null) {
+                synchronized (product) {
+                    product.setPrice(message.getPrice());
+                }
+
+                message.setMessageBytes(("Price of product " + new String(message.getDataBytes()) + " was changed to " + product.getPrice()).getBytes());
+            } else {
+                message.setMessageBytes("Product not found".getBytes());
             }
-
-            message.setMessageBytes(("Price of product " + new String(message.getDataBytes()) + " was changed to " + product.getPrice()).getBytes());
-        } else {
-            message.setMessageBytes("Product not found".getBytes());
+        } finally {
+            productLock.unlock();
         }
     }
 
