fd902bde8a6fa8cf45c2563470fb196dbd5106f9
Anisimov_Eugene <zhecky2004@gmail.com>
2023-06-27 19:36:55 +0300

    Changes by viktorsovyak

--- web/src/components/Goods.tsx
+++ web/src/components/Goods.tsx
@@ -14,27 +14,89 @@ interface Item {
 export default function Goods() {
     const [products, setProducts] = useState<Item[]>([]);
 
+    const handleAddGood = () => {
+        const formWindow = window.open('', '_blank');K
+        formWindow.document.write(`
+            <html>
+            <head>
+                <title>Add Good</title>
+                <style>
+                    body {
+                        font-family: 'Montserrat',serif;
+                    }
+                    .form-container {
+                        max-width: 400px;
+                        margin: 20px auto;
+                        padding: 20px;
+                        border: 1px solid #ccc;
+                        border-radius: 10px;
+                    }
+                    .form-label {
+                        display: block;
+                        margin-bottom: 10px;
+                        font-size: 16px;
+                    }
+                    .form-input {
+                        width: 100%;
+                        padding: 8px;
+                        font-size: 16px;
+                    }
+                    .form-button {
+                        margin-top: 20px;
+                        padding: 10px 20px;
+                        font-size: 16px;
+                        background: #3E7FFF;
+                        color: #FFF;
+                        border: none;
+                        border-radius: 10px;
+                        cursor: pointer;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="form-container">
+                    <h2>Add Good</h2>
+                    <form>
+                        <label class="form-label" for="product-name">Product Name:</label>
+                        <input class="form-input" type="text" id="product-name" name="product-name" required>
+                        
+                        <label class="form-label" for="category-id">Group ID</label>
+                        <input class="form-input" type="text" id="category-id" name="category-id" required>
+                        
+                        <label class="form-label" for="price">Price</label>
+                        <input class="form-input" type="text" id="price" name="price" required>
+                        
+                        <label class="form-label" for="amount">Amount</label>
+                        <input class="form-input" type="text" id="amount" name="amount" required>
+                        
+                        <button class="form-button" type="submit">Submit</button>
+                    </form>
+                </div>
+            </body>
+            </html>
+        `);
+    };
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('http://localhost:8000/api/good/1');
                 const { data } = response;
-                const newData: Item = {
-                    id: 1,
-                    name: data.name,
-                    groupName: 'boba',
-                    quantity: data.count,
-                    price: data.price,
-                };
-                setProducts([newData]); // Set the products state with the fetched data
+                const newProducts = data.map((item: any) => ({
+                    id: item.id,
+                    name: item.name,
+                    groupName: item.groupName,
+                    quantity: item.count,
+                    price: item.price,
+                }));
+                setProducts(newProducts);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
 
-        // Fetch data when the component mounts
         fetchData();
-    }, []); // Empty dependency array ensures the effect is only run once
+    }, []);
 
     return (
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -60,6 +122,7 @@ export default function Goods() {
                         fontFamily: 'Montserrat',
                         padding: '5px 10px',
                     }}
+                    onClick={handleAddGood}
                 >
                     Add product
                 </button>
