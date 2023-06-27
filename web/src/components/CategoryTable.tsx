c76904e52f215b3c2d9c3ff659a47a242a8764e5
Anisimov_Eugene <zhecky2004@gmail.com>
2023-06-27 22:20:23 +0300

    boba

--- web/src/components/CategoryTable.tsx
+++ web/src/components/CategoryTable.tsx
@@ -12,6 +12,13 @@ interface GoodsTableProps {
 }
 
 const CategoryTable: React.FC<GoodsTableProps> = ({ categories }) => {
+    const handleDelete = (id: number) => {
+        console.log(`Delete product with ID ${id}`);
+    };
+
+    const handleEdit = (id: number) => {
+        console.log(`Edit product with ID ${id}`);
+    };
     const data = {
         columns: [
             {
@@ -20,9 +27,47 @@ const CategoryTable: React.FC<GoodsTableProps> = ({ categories }) => {
                 sort: 'asc',
                 width: 150,
             },
+            {
+                label: 'Actions',
+                field: 'actions',
+                width: 100,
+            },
         ],
         rows: categories.map((category) => ({
             name: category.name,
+            actions: (
+                <div>
+                    <button
+                        onClick={() => handleEdit(category.id)}
+                        style={{
+                            marginRight: '5px',
+                            padding: '5px 10px',
+                            fontSize: '14px',
+                            background: '#3E7FFF',
+                            color: '#FFF',
+                            border: 'none',
+                            borderRadius: '5px',
+                            cursor: 'pointer',
+                        }}
+                    >
+                        Edit
+                    </button>
+                    <button
+                        onClick={() => handleDelete(category.id)}
+                        style={{
+                            padding: '5px 10px',
+                            fontSize: '14px',
+                            background: '#FF4136',
+                            color: '#FFF',
+                            border: 'none',
+                            borderRadius: '5px',
+                            cursor: 'pointer',
+                        }}
+                    >
+                        Delete
+                    </button>
+                </div>
+            ),
         })) as any[],
     };
 
