ae5416726639cde6e1de1cb5787144f1976ee27e
ShacksVS <inode16@gmail.com>
2023-06-27 14:15:48 -0500

    add category implementation

--- web/src/components/Categories.tsx
+++ web/src/components/Categories.tsx
@@ -1,6 +1,5 @@
 // @ts-ignore
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import CategoryTable from './CategoryTable.tsx';
 
@@ -9,71 +8,86 @@ interface Item {
     name: string;
 }
 
-
 export default function Categories() {
     const [categories, setCategories] = useState<Item[]>([]);
 
-    const handleAddCategory = () => {
-
+    const handleAddCategory = async () => {
         const formWindow = window.open('', '_blank');
         // @ts-ignore
         formWindow.document.write(`
-            <html>
-            <head>
-                <title>Add Category</title>
+      <html>
+      <head>
+          <title>Add Category</title>
                 <style>
                     body {
-                        font-family: 'Montserrat', serif;
-                        background: linear-gradient(#007bff,pink);
-                    }
+                    font-family: 'Montserrat', serif;
+                    background: linear-gradient(#007bff,pink);
+                }
                     .form-container {
-                        max-width: 400px;
-                        margin: 20px auto;
-                        padding: 20px;
-                        border: 1px solid #ccc;
-                        border-radius: 10px;
-                        background: whitesmoke;
-
-                    }
+                    max-width: 400px;
+                    margin: 20px auto;
+                    padding: 20px;
+                    border: 1px solid #ccc;
+                    border-radius: 10px;
+                    background: whitesmoke;
+                
+                }
                     .form-label {
-                        display: block;
-                        margin-bottom: 10px;
-                        font-size: 16px;
-                    }
+                    display: block;
+                    margin-bottom: 10px;
+                    font-size: 16px;
+                }
                     .form-input {
-                        width: 100%;
-                        padding: 8px;
-                        font-size: 16px;
-                    }
+                    width: 100%;
+                    padding: 8px;
+                    font-size: 16px;
+                }
                     .form-button {
-                        margin-top: 20px;
-                        padding: 10px 20px;
-                        font-size: 16px;
-                        background: #3E7FFF;
-                        color: #FFF;
-                        border: none;
-                        border-radius: 10px;
-                        cursor: pointer;
-                    }
+                    margin-top: 20px;
+                    padding: 10px 20px;
+                    font-size: 16px;
+                    background: #3E7FFF;
+                    color: #FFF;
+                    border: none;
+                    border-radius: 10px;
+                    cursor: pointer;
+                }
                 </style>
-            </head>
-            <body>
-                <div class="form-container">
-                    <h2>Add Category</h2>
-                    <form>
-                        <label class="form-label" for="category-name">Category Name:</label>
-                        <input class="form-input" type="text" id="category-name" name="category-name" required>
-                        
-                        <button class="form-button" type="submit">Submit</button>
-                    </form>
-                </div>
-            </body>
-            </html>
+      </head>
+      <body>
+          <div class="form-container">
+              <h2 style="color: black; font-size: 20px; text-align: center; font-family: 'Arial', sans-serif; font-weight: bold; text-transform: uppercase;">Add Category</h2>
+              <form id="category-form">
+                  <label class="form-label" for="category-name">Category Name:</label>
+                  <input class="form-input" type="text" id="category-name" name="category-name" required>
+                  
+                  <button class="form-button" type="submit">Submit</button>
+              </form>
+          </div>
+          <script>
+            const form = document.getElementById('category-form');
+            form.addEventListener('submit', async (e) => {
+              e.preventDefault();
+              const categoryName = document.getElementById('category-name').value;
+              const data = { name: categoryName };
 
-            
-        `);
-
+              try {
+                const response = await axios.post('http://localhost:8000/api/good', data);
+                // Handle successful response
+                console.log('Category added:', response.data);
+                // Close the form window after successful submission
+                window.close();
+              } catch (error) {
+                console.error('Error adding category:', error);
+              }
+            });
+          </script>
+      </body>
+      </html>
+    `);
     };
+
+    // Fetch categories data
     useEffect(() => {
         const fetchData = async () => {
             try {
